a8fd4c6bc4af71a9d5ed09effbd3109fd79fe689
DSC\ahsoliman <ah@gmail.com>
2018-03-05 12:18:46 +0400

    commit main.cs

---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+using Dsc.Indices.CPI.Tools.BasketImport.Forms;
+
+namespace Dsc.Indices.CPI.Tools.BasketImport
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+           
+        }
+    }
+}
